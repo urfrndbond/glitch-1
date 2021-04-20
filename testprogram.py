03ac2072a8a479ceabc0615fac9ded94b9b57bd4
Shubham <roccobond121@gmail.com>
2021-04-20 08:00:43 +0530

    second version

--- testprogram.py
+++ testprogram.py
@@ -1,2 +1,7 @@
+a=input("Are you 12th pass")
 print("You are awesome")
 print("just a simple addtion")
+if a=="yes":
+    print("You are educated")
+else:
+    print("Everything is Okay but get education")
